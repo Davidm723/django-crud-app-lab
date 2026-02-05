e9c82198ec1494337cf202703cb43c0b1239bdef
Davidm723 <davidmullokandov@gmail.com>
2026-02-04 20:53:25 -0500

    made index page

---
+++ poke_adopter/main_app/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("<h1>Hello World!</h1>")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+class Pokemon:
+    def __init__(self, img, name, types, description, level):
+        self.img = img
+        self.name = name
+        self.types = types
+        self.description = description
+        self.level = level
+
+
+pokemons = [
+    Pokemon(
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+        "Pikachu",
+        "Electric",
+        "Mouse Pokemon",
+        5,
+    ),
+    Pokemon(
+       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/173.png",
+       "Cleffa",
+       'Fairy',
+       'Star Shape Pokemon',
+       2,
+    ),
+    Pokemon(
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/174.png",
+        'Igglybuff',
+        'Normal, Fairy',
+        'Balloon Pokemon',
+        3,
+    ),
+]
+
+def pokemon_index(request):
+    return render(request, 'pokemon/index.html', {'pokemons': pokemons})
