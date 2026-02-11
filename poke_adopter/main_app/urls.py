f88deceb0c836ce2f8ed959aa336be76eda45d3f
Davidm723 <davidmullokandov@gmail.com>
2026-02-10 19:37:00 -0500

    added full crud capability

--- poke_adopter/main_app/urls.py
+++ poke_adopter/main_app/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('pokemon/', views.pokemon_index, name='pokemon_index'),
+    path('pokemon/', views.pokemon_index, name='pokemon-index'),
     path('pokemon/<int:pokemon_id>/', views.pokemon_detail, name='pokemon-detail'),
+    path('pokemon/create', views.PokemonCreate.as_view(), name='pokemon-create'),
+    path('pokemon/<int:pk>/update', views.PokemonUpdate.as_view(), name='pokemon-update'),
+    path('pokemon/<int:pk>/delete', views.PokemonDelete.as_view(), name='pokemon-delete'),
 ]
